68915a2b911209f54c9d87d69e00255c488ac9a3
chengbo <347923936@qq.com>
2020-04-24 01:32:40 +0800

    有一次修改

---
+++ yin/src/test/java/com/ycb/YinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
